6f3598ab21ac11900a768bf2e2ff3504a30d7d67
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-30 11:52:54 +0300

    prepared repository classes

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RestService } from './rest.service';
+import { ProductRepository } from './product.respository';
+import { CategoryRepository } from './category.repository';
 @NgModule({
-    imports: [HttpClientModule]
+  imports: [HttpClientModule],
+  providers: [RestService, ProductRepository, CategoryRepository],
 })
-
 export class ModelModule {}
