e3d23da3d4db0fd3805ed630eceadcd023bbe4de
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-01 14:27:34 +0300

    pagination

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RestService } from './rest.service';
-import { ProductRepository } from './product.respository';
+import { ProductRepository } from './product.repository';
 import { CategoryRepository } from './category.repository';
 @NgModule({
   imports: [HttpClientModule],
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { Category } from "../model/category.model";
 import { CategoryRepository } from "../model/category.repository";
 import { Product } from "../model/product.model";
-import { ProductRepository } from "../model/product.respository";
+import { ProductRepository } from "../model/product.repository";
 
 @Component({
     selector: 'shop',
@@ -13,13 +13,20 @@ import { ProductRepository } from "../model/product.respository";
 
 export class ShopComponent {
     public selectedCategory: Category = null!;
+    public productsPerPage = 3;
+    public selectedPage = 1;
+
+    
 
     constructor(
         private productRepository: ProductRepository,
         private categoryRepository: CategoryRepository) {}
 
         get products(): Product[] {
-            return this.productRepository.getProducts(this.selectedCategory);
+            let index = (this.selectedPage-1) * this.productsPerPage;
+            return this.productRepository
+                    .getProducts(this.selectedCategory)
+                    .slice(index, index + this.productsPerPage);
         }
 
         get categories(): Category[] {
