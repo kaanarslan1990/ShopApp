72571a68c7ab1fe7d381ed7bf6558f5d162c1fec
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-04 13:34:03 +0300

    change product perpage dynamic

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,62 +1,67 @@
-import { Component } from "@angular/core";
-import { Router } from "@angular/router";
-import { Cart } from "../model/cart.model";
-import { Category } from "../model/category.model";
-import { CategoryRepository } from "../model/category.repository";
-import { Product } from "../model/product.model";
-import { ProductRepository } from "../model/product.repository";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cart } from '../model/cart.model';
+import { Category } from '../model/category.model';
+import { CategoryRepository } from '../model/category.repository';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
 
 @Component({
-    selector: 'shop',
-    templateUrl: 'shop.component.html',
-    
+  selector: 'shop',
+  templateUrl: 'shop.component.html',
 })
-
 export class ShopComponent {
-    public selectedCategory: Category = null!;
-    public productsPerPage = 3;
-    public selectedPage = 1;
-    
-    
+  public selectedCategory: Category = null!;
+  public productsPerPage = 3;
+  public selectedPage = 1;
+  public selectedProducts: Product[] = [];
 
-    constructor(
-        private productRepository: ProductRepository,
-        private categoryRepository: CategoryRepository,
-        private cart: Cart,
-        private router: Router ) {}
+  constructor(
+    private productRepository: ProductRepository,
+    private categoryRepository: CategoryRepository,
+    private cart: Cart,
+    private router: Router
+  ) {}
 
-        get products(): Product[] {
-            let index = (this.selectedPage-1) * this.productsPerPage;
-            return this.productRepository
-                    .getProducts(this.selectedCategory)
-                    .slice(index, index + this.productsPerPage);
-        }
+  get products(): Product[] {
+    let index = (this.selectedPage - 1) * this.productsPerPage;
 
-        get pageNumbers(): number[] {
-            return Array(Math.ceil(this.productRepository
-                .getProducts(this.selectedCategory).length/this.productsPerPage))
-                .fill(0)
-                .map((a,i) => i+1)
-        }
+    this.selectedProducts = this.productRepository.getProducts(
+      this.selectedCategory
+    );
 
-        changePage(p: number) {
-            this.selectedPage = p;
-        }
+    return this.selectedProducts.slice(index, index + this.productsPerPage);
+  }
 
-        
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.productRepository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((a, i) => i + 1);
+  }
 
-        get categories(): Category[] {
-            return this.categoryRepository.getCategories();
-        }
+  changePage(p: number) {
+    this.selectedPage = p;
+  }
+  changePageSize(size: number) {
+    this.productsPerPage = size;
+    this.changePage(1);
+  }
 
-        changeCategory(newCategory?: Category ) {
-            this.selectedCategory = newCategory!;
-        }
+  get categories(): Category[] {
+    return this.categoryRepository.getCategories();
+  }
 
-        addProductToCart(product: Product) {
-            this.cart.addItem(product);
-            this.router.navigateByUrl('/cart');
+  changeCategory(newCategory?: Category) {
+    this.selectedCategory = newCategory!;
+  }
 
-        }
-    
+  addProductToCart(product: Product) {
+    this.cart.addItem(product);
+    this.router.navigateByUrl('/cart');
+  }
 }
