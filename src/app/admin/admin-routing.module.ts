76bd18ead74952e0a1879fccb8567f02d6d45329
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-07-05 10:59:52 +0300

    admin pages created

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Category } from '../model/category.model';
+import { CategoryListComponent } from '../shop/category-list/category-list.component';
+import { ProductListComponent } from '../shop/product-list/product-list.component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
+import { CategoryFormComponent } from './categories/category-form/category-form.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
 
 const routes: Routes = [
-
-  {path: 'auth', component: AuthComponent},
-  {path: 'main', component: AdminComponent, canActivate: [AuthGuard]},
-  {path: '**', redirectTo: 'auth'},
+  { path: 'auth', component: AuthComponent },
+  {
+    path: 'main', component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "products/:mode/:id", component: ProductFormComponent },
+      { path: "products/:mode", component: ProductFormComponent },
+      { path: "products", component: ProductListComponent },
+      { path: "categories/:mode/:id", component: CategoryFormComponent },
+      { path: "categories/:mode", component: CategoryFormComponent },
+      { path: "categories", component: CategoryListComponent },
+      { path: "orders", component: OrderListComponent }
+    ]
+  },
+  { path: '**', redirectTo: 'auth' }
 ];
 
 @NgModule({
--- src/app/admin/auth.guard.ts
+++ src/app/admin/auth.guard.ts
@@ -1,17 +1,18 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterState, RouterStateSnapshot } from "@angular/router";
-import { AuthService } from "../model/auth.service";
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../model/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router, private authService: AuthService) {}
 
-    canActivate(route:ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    constructor(private router: Router, private authService: AuthService){}
+    
+    canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
+
         if(!this.authService.authenticated) {
             this.router.navigateByUrl('/admin/auth');
             return false;
         }
         return true;
-
     }
 }
